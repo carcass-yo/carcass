9b6c8e4d590a01fb7f989d5b51cb675381d718fd
Constantine Karnaukhov <1119@zolotoykod.ru>
2018-01-12 14:09:52 +0400

    refactor(project): move tools (npm to yarn, jest to mocha)

--- test/app.js
+++ test/app.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
+const describe = require('mocha').describe;
+const before = require('mocha').before;
+const it = require('mocha').it;
 
 describe('generator-carcass:app', () => {
-  beforeAll(() => {
+  before(() => {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({stack: 'web'});
+      .withPrompts({stack: 'web'})
+      .toPromise();
   });
 
   it('creates files', () => {
